da092c572f90ccd6f5f8da84e770534f6d9d93a6
eskalona17 <eskalona_17@hotmail.com>
2020-04-23 10:12:15 +0200

    todolist creado

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <form id="app">
+        <label for="tast" class="label">tarea: </label>
+        <input type="text" v-model="newTask" id="id">{{newTask}}
+        <input type="submit" value="Crear tarea">
+    </form>
+</template>
+<script>
+export default {
+    data: () => ({
+        newTask: ''
+    })
+}
+</script>
+
+<style>
+
+</style>
