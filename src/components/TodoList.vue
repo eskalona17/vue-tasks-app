c63c9b8511c3c5d1b6617be7491c1e79487cd6bc
eskalona17 <eskalona_17@hotmail.com>
2020-04-23 10:53:38 +0200

    añadiendo estilos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,14 @@
 <template>
-  <form id="app" @submit.prevent="createTask">
-    <label for="tast" class="label">tarea: </label>
-    <input type="text" v-model="newTask" id="id" />
-    <input type="submit" value="Crear tarea" />
-    <ul>
+  <div class="task-list">
+    <h1>{{ tasks.length }}</h1>
+    <form class="form" id="app" @submit.prevent="createTask">
+      <label for="tast" class="label">tarea: </label>
+      <input class="input" type="text" v-model="newTask" id="id" />
+      <input class="button" type="submit" value="Crear tarea" />
+    </form>
+    <ul class="list">
       <li
+        class="task"
         v-for="(task, i) in tasks"
         :key="'task' + i"
         :class="{ completed: task.completed }"
@@ -13,7 +17,7 @@
         {{ task.text }}
       </li>
     </ul>
-  </form>
+  </div>
 </template>
 <script>
 export default {
@@ -31,21 +35,57 @@ export default {
       this.newTask = "";
       console.log(this.tasks);
     },
-    completedTask(taskText){
-        for(let i = 0; i < this.tasks.length; i++){
-            let task = this.tasks[i];
-            if(taskText === task.text) {
-                task.completed = !task.completed
-            }
+    completedTask(taskText) {
+      for (let i = 0; i < this.tasks.length; i++) {
+        let task = this.tasks[i];
+        if (taskText === task.text) {
+          task.completed = !task.completed;
         }
-    }
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
+.task-list {
+  width: 800px;
+  max-width: 100%;
+  margin: 0px auto;
+}
+.form {
+  background: white;
+  border-radius: 12px;
+  padding: 30px;
+  box-shadow: 0px 10px 22px -1px rgba(0, 0, 0, 0.25);
+  margin-top: 10px;
+}
+.label {
+  display: block;
+  margin-bottom: 10px;
+}
+.input {
+  height: 35px;
+}
+.button {
+  margin-left: 20px;
+  height: 35px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+  background-color: #2ecc71;
+  color: #ecf0f1;
+  cursor: pointer;
+}
+.list {
+  margin-top: 40px;
+}
+.task {
+  cursor: pointer;
+  margin: 10px 0;
+}
 .completed {
   text-decoration: line-through;
-  color: grey;
+  color: lightgrey;
 }
 </style>
