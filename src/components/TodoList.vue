f81e6f8ed34cd700f34fac804bec4645f64e5695
eskalona17 <eskalona_17@hotmail.com>
2020-04-23 10:21:44 +0200

    tareas creadas

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,28 @@
 <template>
-    <form id="app">
-        <label for="tast" class="label">tarea: </label>
-        <input type="text" v-model="newTask" id="id">{{newTask}}
-        <input type="submit" value="Crear tarea">
-    </form>
+  <form id="app" @submit.prevent="createTask">
+    <label for="tast" class="label">tarea: </label>
+    <input type="text" v-model="newTask" id="id" />{{ newTask }}
+    <input type="submit" value="Crear tarea" />
+  </form>
 </template>
 <script>
 export default {
-    data: () => ({
-        newTask: ''
-    })
-}
+  data: () => ({
+    newTask: "",
+    tasks: []
+  }),
+  methods: {
+      createTask() {
+          let task = {
+              text: this.newTask,
+              completed: false
+          };
+          this.tasks.push(task);
+          this.newTask = '';
+          console.log(this.tasks);
+      }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
